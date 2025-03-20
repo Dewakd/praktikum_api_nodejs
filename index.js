b495e9417eb75c4fe14e2722aae58b2872101179
Dewakd <86773325+Dewakd@users.noreply.github.com>
2025-03-20 22:15:28 +0800

    menambahkan endpoint users supaya bisa melakukan CRUD

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ const db = mysql.createConnection({
     host: 'localhost', 
     user: 'root', 
     database: 'openapi', 
-    password: "" 
+    password: ""
 });
 
 const app = express();
 app.use(express.json());
+
+app.use('/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/v1/users', (req, res) => {
     db.query('SELECT * FROM users', (err, results) => {
@@ -22,7 +24,58 @@ app.get('/v1/users', (req, res) => {
             res.status(500).send('Internal Server Error');
             return;
         }
-        res.status(200).send("Success");
         res.json(results);
     });
 });
+
+app.get('/v1/user/:id', (req, res) => {
+    db.query('SELECT * FROM users WHERE id = ?', [req.params.id], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.put('/v1/user/:id', (req, res) => {
+    const { name, email, age } = req.body;
+    db.query(
+        'UPDATE users SET name = ?, email = ?, age = ? WHERE id = ?', 
+        [name, email, age, req.params.id], 
+        (err, results) => {
+            if (err) {
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+
+            res.json(results);
+        }
+    )
+});
+
+app.delete('/v1/user/:id', (req, res) => {
+    db.query('DELETE FROM users WHERE id = ?', [req.params.id], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.post('/v1/user', (req, res) => {
+    const { name, email, age } = req.body;
+    db.query('INSERT INTO users (name, email, age) VALUES (?, ?, ?)', [name, email, age], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
